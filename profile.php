9d7c205165491a9b519110b1cb21147f6ed9687e
AdliXSec <naufalsyahruradli@gmail.com>
2024-12-02 08:50:27 +0700

    add photo

--- profile.php
+++ profile.php
@@ -57,6 +57,12 @@ if (!isset($_SESSION['id_user'])) {
    
   <main class="container opacity-80 mx-auto absolute z-20 mt-4 p-4">
     <div class="bg-white shadow-md rounded-lg p-4">
+        <a href="index.php">
+        <svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" fill="currentColor" class="bi bi-backspace" viewBox="0 0 16 16">
+        <path d="M5.83 5.146a.5.5 0 0 0 0 .708L7.975 8l-2.147 2.146a.5.5 0 0 0 .707.708l2.147-2.147 2.146 2.147a.5.5 0 0 0 .707-.708L9.39 8l2.146-2.146a.5.5 0 0 0-.707-.708L8.683 7.293 6.536 5.146a.5.5 0 0 0-.707 0z"/>
+        <path d="M13.683 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-7.08a2 2 0 0 1-1.519-.698L.241 8.65a1 1 0 0 1 0-1.302L5.084 1.7A2 2 0 0 1 6.603 1zm-7.08 1a1 1 0 0 0-.76.35L1 8l4.844 5.65a1 1 0 0 0 .759.35h7.08a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1z"/>
+        </svg>
+        </a>
       <h2 class="text-xl font-semibold mb-2 text-center pb-4">Profile Kamu</h2>
       <div class="flex justify-center gap-2 pb-4">
       <img src="img/<?php if($row['image_user'] == '-'){echo 'user.png';}else{echo 'profile/'.$row['image_user'];}?>?>" alt="" class="w-[70px] h-[70px] rounded-full">
